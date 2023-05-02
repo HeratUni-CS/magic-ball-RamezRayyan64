b8c47b14381b17562c1a94ce4b4d653b98134871
RamezRayyan64 <ramez.rayyan64@gmail.com>
2023-05-02 23:20:24 +0430

    assignment finished

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: const Text('Magic Ball'),
+          backgroundColor: Colors.blue.shade900,
+        ),
+        body: Center(
+          child: TextButton(
+            onPressed: () {
+              setState(() {
+                ballNumber = Random().nextInt(5) + 1;
+              });
+            },
+            child: Image.asset('images/ball$ballNumber.png'),
+          ),
+        ),
+      ),
+    );
+  }
+}
